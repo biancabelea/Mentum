9dee5a7108c375c50470e4775eec15228f0c0fc9
biancabelea <80485735+biancabelea@users.noreply.github.com>
2024-12-08 21:21:29 +0200

    MongoDB setup

--- mentum-backend/server.js
+++ mentum-backend/server.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// // MongoDB connection
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-//   .then(() => console.log('MongoDB connected'))
-//   .catch((err) => console.error('Database connection error:', err));
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('Database connection error:', err));
 
 app.get('/', (req, res) => res.send('API is running...'));
 

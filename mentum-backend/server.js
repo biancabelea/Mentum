9b471913f54c09d63581c90a918a1f762b8256f2
biancabelea <80485735+biancabelea@users.noreply.github.com>
2025-01-09 23:07:27 +0200

    Update mongo

--- mentum-backend/server.js
+++ mentum-backend/server.js
@@ -23,6 +23,6 @@ app.use('/', authRoutes);
 
 // MongoDB Connection and Server Start
 mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.MONGO_URI)
     .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
     .catch(err => console.error('MongoDB connection error:', err));
